4375b0a5518df338fd4ac10353b4b70333382e5e
Craig Huizenga <hidatch@gmail.com>
2016-05-24 15:28:40 -0700

    Rip things apart into modules

--- dalet_serializer.py
+++ dalet_serializer.py
@@ -1,112 +1,22 @@
 __author__ = 'hidat'
 
-from yattag import Doc, indent
-import os.path as path
 import os
 import argparse
-import csv
-
-class DaletSerializer:
-
-    def __init__(self, output_dir):
-        # Set up metadata directories
-        self.release_meta_dir = output_dir
-        if not path.exists(self.release_meta_dir):
-            os.makedirs(self.release_meta_dir)
-        print("Release meta: ", self.release_meta_dir)
-
-
-    def saveRelease(self, release):
-        """
-        Create an XML file of release metadata that Dalet will be happy with
-        
-        :param release: Processed release metadata from MusicBrainz
-        """
-
-        output_dir = self.release_meta_dir
-
-        doc, tag, text = Doc().tagtext()
-
-        doc.asis('<?xml version="1.0" encoding="UTF-8"?>')
-        with tag('Titles'):
-            with tag('GlossaryValue'):
-                with tag('Key1'):
-                    text(release.mbID)
-                with tag('ItemCode'):
-                    text(release.mbID)
-                with tag('KEXPReviewRich'):
-                    text(release.daletReview)
-        formatted_data = indent(doc.getvalue())
-
-        output_file = path.join(output_dir, 'r' + release.mbID + ".xml")
-        with open(output_file, "wb") as f:
-            f.write(formatted_data.encode("UTF-8"))
-
-class Review:
-
-    def __init__(self, csvParts):
-        if csvParts is not None:
-            if len(csvParts) > 1:
-                #header = "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % ('Current Rotation', 'Review Title', 'Review', 'Dalet Review', 'Reviewed By', 'One Star', 'Two Star', 'Three Star', 'Filename', 'Artist Credit', 'Label')
-                self.rotation = csvParts[0]
-                self.name = csvParts[1]
-                self.review = csvParts[2]
-                self.daletReview = csvParts[3]
-                self.reviewedBy = csvParts[4]
-                self.filename = csvParts[8]
-                self.artistCredit = csvParts[9]
-                self.label = csvParts[10]
-                self.mbID = csvParts[11].strip()
-                self.oneStarTracks = csvParts[5].split(',')
-                self.twoStarTracks = csvParts[6].split(',')
-                self.threeStarTracks = csvParts[7].split(',')
-
-        else:
-            self.filename = None
-            self.rotation = None
-            self.name = None
-            self.artistCredit = None
-            self.label = None
-            self.review = None
-            self.trackList = None
-            self.tracks = None
-            self.reviewedBy = ''
-            self.oneStarTracks = []
-            self.twoStarTracks = []
-            self.threeStarTracks = []
-            self.mbID = None
-
-    @staticmethod
-    def readCSV(filename):
-        reviews = []
-        f = open(filename, 'r')
-        csvReader = csv.reader(open(filename, newline=''), delimiter='\t', quotechar='"')
-        firstLine = True
-        for line in csvReader:
-            if not firstLine:
-                review = Review(line)
-                if review is not None:
-                    reviews.append(review)
-            else:
-                firstLine = False
-
-        return reviews
-
-
+from rlib import parser, serializer
 
 def main():
-    parser = argparse.ArgumentParser(description='Processes a KEXP weekly review documents into a spreadsheet.')
-    parser.add_argument('input_file', help="Directory or file to process.  Only docx files are will be processed.")
-    parser.add_argument('output_directory', help="Name for the tab-delimited file that will be created to store the results in.")
-    #parser.add_argument('-d', '--delete', default=False, const=True, nargs='?', help="Delete audio files from input_file after processing")
+    argParser = argparse.ArgumentParser(description='Processes a KEXP weekly review documents into a spreadsheet.')
+    argParser.add_argument('input_file', help="Directory or file to process.  Only docx files are will be processed.")
+    argParser.add_argument('output_directory', help="Name for the tab-delimited file that will be created to store the results in.")
+    #argParser.add_argument('-d', '--delete', default=False, const=True, nargs='?', help="Delete audio files from input_file after processing")
 
-    args = parser.parse_args()
+    args = argParser.parse_args()
 
     if os.path.isfile(args.input_file):
         successCount = 0
         errorCount = 0
-        reviews = Review.readCSV(args.input_file)
-        fp = DaletSerializer(args.output_directory)
+        reviews = parser.CSVFile.readFile(args.input_file)
+        fp = serializer.DaletSerializer(args.output_directory)
         for review in reviews:
             if review.mbID is not None and review.mbID > '':
                 fp.saveRelease(review)
--- review_processor.py
+++ review_processor.py
@@ -1,239 +1,13 @@
-import mammoth
-import re
 import os
 import argparse
-import unicodedata
-import html
 import operator
-
-
-class Track:
-    reNumberExtract = re.compile(r"(\d+)")
-
-    def __init__(self, rawTrack):
-        self.rawTrack = rawTrack
-        self.stars = 1
-        m = self.reNumberExtract.search(rawTrack)
-
-        if m:
-            self.trackNum = m.group(0)
-        else:
-            self.trackNum = None
-
-        if "<em>" in rawTrack:
-            self.stars +=1
-        if "<strong>" in rawTrack:
-            self.stars += 1
-
-class Album:
-    reTrackSplit = re.compile(r",|&amp;")
-    reReviewer = re.compile(r"[-]+([\w\s]+)")
-
-    def __init__(self, filename, rotation):
-        self.filename = filename
-        self.rotation = rotation
-        self.name = None
-        self.artistCredit = None
-        self.label = None
-        self.review = None
-        self.trackList = None
-        self.tracks = None
-        self.reviewedBy = ''
-        self.oneStarTracks = []
-        self.twoStarTracks = []
-        self.threeStarTracks = []
-
-    @staticmethod
-    def isNameString(nameStr):
-        if len(nameStr) > 200:
-            return False
-
-        # Checks if the string is in the format of [ALBUM] - [ARTIST CREDIT] ([LABEL])
-        labelStart = nameStr.rfind('(')
-        if labelStart <= 0:
-            return False
-
-        s = nameStr[:(labelStart-1)]
-        if s.find('- ') > -1:
-            return True
-
-        if s.find(' -') > -1:
-            return True
-
-        if s.find(': ') > -1:
-            return True
-
-        return False
-
-    def parseNameString(self, nameStr):
-        # Formatted as [ALBUM] - [ARTIST CREDIT] ([LABEL])
-        nameStr = html.unescape(nameStr)
-        nameStr = re.sub("<.*?>", "", nameStr)
-        #nameStr = nameStr.replace("&amp;", '&')
-        labelStart = nameStr.rfind('(')
-        if labelStart > 0:
-            self.label = nameStr[labelStart+1:-1]
-
-        parts = nameStr[:(labelStart-1)].split('- ')
-        if len(parts) == 1:
-            parts = nameStr[:(labelStart-1)].split(' -')
-        if len(parts) == 1:
-            parts = nameStr[:(labelStart-1)].split(': ')
-
-        if len(parts) == 1:
-            self.name = parts[0]
-        elif len(parts) > 1:
-            self.artistCredit = parts[0].strip()
-            self.name = parts[1].strip()
-            if len(parts) > 2:
-                self.name += ' - ' + parts[2]
-
-    ###
-    # Parses a paragraph that we are expecting to be a 'review'
-    # This can be called multiple times (because sometimes we get newlines in the middle of a review).  Multiples lines
-    # are appended together.
-    ###
-    def parseReviewString(self, reviewStr):
-        if self.review is None:
-            self.review = reviewStr
-        else:
-            self.review += ' ' + reviewStr
-
-        sentences = reviewStr.split('.')
-        tl = None
-        rb = None
-        if len(sentences) > 1:
-            rb = sentences[-1].strip()
-            if rb.startswith('-'):
-                rm = self.reReviewer.findall(rb)
-                if len(rm) > 0:
-                    self.reviewedBy = rm[0]
-            elif rb.startswith('Try'):
-                parts = rb.split('-')
-                if len(parts) > 1:
-                    tl = parts[0]
-                    rm = self.reReviewer.findall(rb)
-                    if len(rm) > 0:
-                        self.reviewedBy = rm[0]
-            else:
-                rb = None
-
-        if tl is None and len(sentences) > 2:
-            tl = sentences[-2].strip()
-        if (not (tl is None)) and tl.startswith('Try'):
-                self.trackList = tl
-
-                self.tracks = []
-                rawTracks = self.reTrackSplit.split(self.trackList)
-                for t in rawTracks:
-                    track = Track(t)
-                    self.tracks.append(track)
-                    if track.stars == 3:
-                        self.threeStarTracks.append(track)
-                    elif track.stars == 2:
-                        self.twoStarTracks.append(track)
-                    else:
-                        self.oneStarTracks.append(track)
-
-    def print(self):
-        print("\n%s: %s by [%s] (on %s) - %s" %(self.rotation, self.name, self.artistCredit, self.label, self.review))
-        print("Reviewed by: %s.  Tracks to try: %s" % (self.reviewedBy, self.trackList))
-        for t in self.tracks:
-            print("%s: %d" % (t.trackNum, t.stars))
-
-    def formatCSV(self):
-        oneStar = ''
-        for t in self.oneStarTracks:
-            if not (t.trackNum is None):
-                if oneStar > '':
-                    oneStar += ', '
-                oneStar += str(t.trackNum)
-
-        twoStar = ''
-        for t in self.twoStarTracks:
-            if not (t.trackNum is None):
-                if twoStar > '':
-                    twoStar += ', '
-                twoStar += str(t.trackNum)
-
-        threeStar = ''
-        for t in self.threeStarTracks:
-            if not (t.trackNum is None):
-                if threeStar > '':
-                    threeStar += ', '
-                threeStar += str(t.trackNum)
-
-        decodedReview = html.unescape(self.review)
-        textReview = re.sub("<.*?>", "", decodedReview)
-        decodedReview = decodedReview.replace("em>", "u>").replace("strong>", "b>")
-        encodedReview = html.escape(decodedReview, False)
-        s = "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (self.rotation, self.name, textReview, encodedReview, self.reviewedBy, oneStar, twoStar, threeStar, self.filename, self.artistCredit, self.label)
-        return s
-    
-class FileProcessor:
-    def __init__(self, filename):
-        self.filename = filename
-        self.albums = []
-
-    def process(self):
-        FileProcessor.processFile(self.filename, self.albums)
-
-    def exportAlbums(self, targetFilename):
-        file = open(targetFilename, "w", encoding='utf-8')
-        sortedAlbums = sorted(self.albums, key=operator.attrgetter('filename', 'rotation', 'name'))
-        for a in sortedAlbums:
-            s = a.formatCSV()
-            file.write(s)
-            file.write('\n')
-        file.close()
-
-    @staticmethod
-    def processFile(filename, albums):
-        style_map = "u => em"
-
-        with open(filename, "rb") as docx_file:
-            result = mammoth.convert_to_html(docx_file, style_map=style_map)
-            html = result.value # The generated HTML
-            paras = html.split('<p>')
-            currentRotation = None
-
-            album = None
-            lastAlbum = None
-
-            for p in paras:
-                s = p[:-4].strip()
-                #s = s.replace(u'\xa0', ' ').replace(u'\u2013', '-')
-                s = unicodedata.normalize('NFKC', s).replace(u'\u2013', '-')
-                if len(s) > 0 and s[0] != '<':
-                    if len(s) > 0:
-                        if len(s) < 10:
-                            s = s[:-1].rstrip()
-                            if s=='H' or s=='M' or s=='L' or s=='R/N':
-                                currentRotation = s
-                                albumName = None
-                                albumReview = None
-                                waitingForAlbum = True
-                        else:
-                            if album is None:
-                                if Album.isNameString(s):
-                                    album = Album(filename, currentRotation)
-                                    album.parseNameString(s)
-                                    lastAlbum = None
-                                elif not (lastAlbum is None):
-                                    # did somebody put a newline in the middle of a review? Try to add it to the last album
-                                    lastAlbum.parseReviewString(s)
-                            else:
-                                album.parseReviewString(s)
-                                albums.append(album)
-                                lastAlbum = album
-                                album = None
+from rlib import parser
 
 
 class DirectoryProcessor:
     def __init__(self, directoryPath):
         self.directoryPath = directoryPath
         self.albums = []
-
 
     def processDirectory(self):
         path_start = len(self.directoryPath) + 1
@@ -246,7 +20,7 @@ class DirectoryProcessor:
                 if src_name[-4:].lower() == 'docx' and src_name[:1] != '~':
                     file_name = os.path.join(root, src_name)
                     print(src_name)
-                    FileProcessor.processFile(file_name, self.albums)
+                    parser.DocParser.processFile(file_name, self.albums)
 
     def exportAlbums(self, targetFilename):
         file = open(targetFilename, "w", encoding='utf-8')
@@ -260,15 +34,15 @@ class DirectoryProcessor:
         file.close()
 
 def main():
-    parser = argparse.ArgumentParser(description='Processes a KEXP weekly review documents into a spreadsheet.')
-    parser.add_argument('input_directory', help="Directory or file to process.  Only docx files are will be processed.")
-    parser.add_argument('output_file', help="Name for the tab-delimited file that will be created to store the results in.")
-    #parser.add_argument('-d', '--delete', default=False, const=True, nargs='?', help="Delete audio files from input_directory after processing")
+    argParser = argparse.ArgumentParser(description='Processes a KEXP weekly review documents into a spreadsheet.')
+    argParser.add_argument('input_directory', help="Directory or file to process.  Only docx files are will be processed.")
+    argParser.add_argument('output_file', help="Name for the tab-delimited file that will be created to store the results in.")
+    #argParser.add_argument('-d', '--delete', default=False, const=True, nargs='?', help="Delete audio files from input_directory after processing")
 
-    args = parser.parse_args()
+    args = argParser.parse_args()
 
     if os.path.isfile(args.input_directory):
-        fp = FileProcessor(args.input_directory)
+        fp = parser.DocParser(args.input_directory)
         fp.process()
         fp.exportAlbums(args.output_file)
     else:
